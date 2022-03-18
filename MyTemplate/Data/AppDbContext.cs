74b75f2ecbe49e38f3dc7d2136cae52cdb653ac2
HocLT <hdquang6@gmail.com>
2022-03-18 11:10:44 +0700

    add identity db

---
+++ MyTemplate/Data/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTemplate.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // phần này sẽ xử lý việc loại bỏ tiền tố AspNet của bảng được sinh ra
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // builder.Model.GetEntityTypes(): lấy về các entities 
+            foreach (var entityType in builder.Model.GetEntityTypes())
+            {
+                // lấy về table name trong entity
+                string tableName = entityType.GetTableName();
+                if (tableName.StartsWith("AspNet"))
+                {
+                    // đổi lại tên table
+                    entityType.SetTableName(tableName.Substring(6));    // bỏ 6 ký tự đầu là "AspNet"
+                }
+            }
+        }
+    }
+}
--- MyTemplate/Startup.cs
+++ MyTemplate/Startup.cs
@@ -1,8 +1,11 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MyTemplate.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,6 +25,59 @@ namespace MyTemplate
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
+            services.AddDbContext<AppDbContext>(o => {
+                o.UseSqlServer(Configuration.GetConnectionString("MyTemplateConnectionString"));
+            });
+
+            services.AddIdentity<AppUser, IdentityRole>(o => {
+                o.SignIn.RequireConfirmedEmail = false;
+            })
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            // Truy cập IdentityOptions
+            services.Configure<IdentityOptions>(options => {
+                // Thiết lập về Password
+                options.Password.RequireDigit = false; // Không bắt phải có số
+                options.Password.RequireLowercase = false; // Không bắt phải có chữ thường
+                options.Password.RequireNonAlphanumeric = false; // Không bắt ký tự đặc biệt
+                options.Password.RequireUppercase = false; // Không bắt buộc chữ in
+                options.Password.RequiredLength = 3; // Số ký tự tối thiểu của password
+                options.Password.RequiredUniqueChars = 1; // Số ký tự riêng biệt
+
+                // Cấu hình Lockout - khóa user
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5); // Khóa 5 phút
+                options.Lockout.MaxFailedAccessAttempts = 5; // Thất bại 5 lầ thì khóa
+                options.Lockout.AllowedForNewUsers = true;
+
+                // Cấu hình về User.
+                options.User.AllowedUserNameCharacters = // các ký tự đặt tên user
+                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = true; // Email là duy nhất
+
+                // Cấu hình đăng nhập.
+                options.SignIn.RequireConfirmedEmail = true; // Cấu hình xác thực địa chỉ email (email phải tồn tại)
+                options.SignIn.RequireConfirmedPhoneNumber = false; // Xác thực số điện thoại
+            });
+
+            // Cấu hình Cookie
+            services.ConfigureApplicationCookie(options => {
+                // options.Cookie.HttpOnly = true;  
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+                options.LoginPath = $"/login/";                                 // Url đến trang đăng nhập
+                options.LogoutPath = $"/logout/";
+                options.AccessDeniedPath = $"/Identity/Account/AccessDenied";   // Trang khi User bị cấm truy cập
+            });
+            services.Configure<SecurityStampValidatorOptions>(options =>
+            {
+                // Trên 5 giây truy cập lại sẽ nạp lại thông tin User (Role)
+                // SecurityStamp trong bảng User đổi -> nạp lại thông tin Security
+                options.ValidationInterval = TimeSpan.FromSeconds(5);
+            });
+            services.AddOptions();
+
+            services.AddRazorPages();
             services.AddControllersWithViews();
         }
 
@@ -47,6 +103,9 @@ namespace MyTemplate
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                // for razor page
+                endpoints.MapRazorPages();
             });
         }
     }
